610da6d76e52ef162c496bf1817a0596010b345a
Takeru Ohta <phjgt308@gmail.com>
2025-07-19 22:29:35 +0900

    Add keylabels field to Config struct with key input parsing

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,13 @@
 use std::collections::BTreeMap;
 
-use crate::action::Action;
+use tuinix::KeyInput;
+
+use crate::{action::Action, mame};
 
 #[derive(Debug, Clone)]
 pub struct Config {
     pub commands: BTreeMap<String, Action>,
+    pub keylabels: BTreeMap<KeyInput, String>,
 }
 
 impl<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>> for Config {
@@ -13,6 +16,14 @@ impl<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>> for Config {
     fn try_from(value: nojson::RawJsonValue<'text, 'raw>) -> Result<Self, Self::Error> {
         Ok(Self {
             commands: value.to_member("commands")?.required()?.try_into()?,
+            keylabels: value
+                .to_member("keylabels")?
+                .map(|v| {
+                    v.to_object()?
+                        .map(|(k, v)| Ok((mame::parse_key_input(k)?, v.try_into()?)))
+                        .collect()
+                })?
+                .unwrap_or_default(),
         })
     }
 }
