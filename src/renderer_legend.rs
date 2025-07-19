4ede4253f3db64f55094d7fff460946f18c8a6e5
Takeru Ohta <phjgt308@gmail.com>
2025-07-19 13:06:56 +0900

    Refactor LegendRenderer to dynamically calculate size with SizeDetector helper

--- src/renderer_legend.rs
+++ src/renderer_legend.rs
@@ -1,7 +1,7 @@
 use std::fmt::Write;
 
 use orfail::OrFail;
-use tuinix::{TerminalRegion, TerminalSize};
+use tuinix::{TerminalPosition, TerminalRegion, TerminalSize};
 
 use crate::{TerminalFrame, state::State};
 
@@ -9,16 +9,53 @@ use crate::{TerminalFrame, state::State};
 pub struct LegendRenderer;
 
 impl LegendRenderer {
-    pub fn render(&self, _state: &State, frame: &mut TerminalFrame) -> orfail::Result<()> {
-        let style = tuinix::TerminalStyle::new().dim();
-        let reset = tuinix::TerminalStyle::RESET;
-
-        write!(frame, "{style}^C: Quit | ^S: Save | ^O: Open{reset}").or_fail()?;
-
+    pub fn render(&self, state: &State, frame: &mut TerminalFrame) -> orfail::Result<()> {
+        self.render_to_writer(state, frame).or_fail()?;
         Ok(())
     }
 
-    pub fn region(&self, _state: &State, size: TerminalSize) -> TerminalRegion {
-        size.to_region().take_top(1).take_right(40)
+    pub fn region(&self, state: &State, size: TerminalSize) -> TerminalRegion {
+        let mut detector = SizeDetector::default();
+        self.render_to_writer(state, &mut detector).expect("bug");
+        if !size.contains(detector.size.to_region().bottom_right()) {
+            TerminalRegion::default()
+        } else {
+            size.to_region()
+                .take_top(detector.size.rows)
+                .take_right(detector.size.cols)
+        }
+    }
+
+    fn render_to_writer<W: Write>(&self, _state: &State, mut writer: W) -> orfail::Result<()> {
+        writeln!(writer, "^C: Quit | ^S: Save | ^O: Open ").or_fail()?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Default)]
+struct SizeDetector {
+    cursor: TerminalPosition,
+    size: TerminalSize,
+}
+
+impl Write for SizeDetector {
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        for ch in s.chars() {
+            match ch {
+                '\n' => {
+                    self.size.rows += 1;
+                    self.size.cols = self.size.cols.max(self.cursor.col);
+                    self.cursor.col = 0;
+                    self.cursor.row += 1;
+                }
+                _ => {
+                    self.cursor.col += 1;
+                }
+            }
+        }
+        self.size.cols = self.size.cols.max(self.cursor.col);
+
+        Ok(())
     }
 }
